d65b12619f8c2c7afd17ca3fc5590dc35ab398cd
Adam Spiers <adam@celtic.linksys.moosehall>
2022-11-15 15:56:42 +0000

    basic findProject

--- src/commands/linear-deps.ts
+++ src/commands/linear-deps.ts
@@ -1,11 +1,74 @@
-import { GluegunCommand } from 'gluegun'
+import { GluegunCommand, print, http } from 'gluegun'
+
+async function findProject(api, projectSubstring) {
+  const { ok, data } = await api.post('/graphql', {
+    query: `
+      query Projects($filter: ProjectFilter) {
+        projects(filter: $filter) {
+          nodes {
+            id
+            slugId
+            name
+            description
+          }
+        }
+      }
+    `,
+    variables: {
+      filter: {
+        name: {
+          contains: projectSubstring,
+        },
+      },
+    },
+  })
+
+  if (!ok) {
+    print.error(data)
+    return null
+  }
+
+  if (!data.data) {
+    print.error(`Got no data field in response: ${data}`)
+    return null
+  }
+
+  if (!data.data.projects) {
+    print.error(`Got no data.projects field in response: ${data}`)
+    return null
+  }
+
+  if (!data.data.projects.nodes) {
+    print.error(`Got no data.projects.nodes field in response: ${data}`)
+    return null
+  }
+
+  const projects = data.data.projects.nodes
+  if (projects.length != 1) {
+    print.warning(`Found ${projects.length} projects:`)
+    for (const project of projects) {
+      print.warning(`  ${project.name}`)
+    }
+    return null
+  }
+
+  return projects[0]
+}
 
 const command: GluegunCommand = {
   name: 'linear-deps',
-  run: async (toolbox) => {
-    const { print } = toolbox
+  run: async () => {
+    const api = http.create({
+      baseURL: 'https://api.linear.app',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: process.env.LINEAR_API_KEY,
+      },
+    })
 
-    print.info('Welcome to your CLI')
+    const project = await findProject(api, 'Celo Retirements')
+    if (!project) return
+    print.info(project)
   },
 }
 
